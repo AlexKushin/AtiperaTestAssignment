e2a575768246759057e2a94a91083ff55b494de4
kyshin <kyshin1993@gmail.com>
2024-08-08 19:18:44 +0200

    first commit

---
+++ src/main/java/com/atipera/okushyn/testassignment/AtiperaTestAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.atipera.okushyn.testassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtiperaTestAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtiperaTestAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atipera/okushyn/testassignment/AtiperaTestAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atipera.okushyn.testassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtiperaTestAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
