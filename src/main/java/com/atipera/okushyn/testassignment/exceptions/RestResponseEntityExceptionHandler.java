374e459400b9a04f4b4f364ae53da38e9d912726
kyshin <kyshin1993@gmail.com>
2024-08-12 20:50:42 +0200

    ExceededRateLimitException has been added RestResponseEntityExceptionHandler - exception handlers for HttpClientErrorException and ExceededRateLimitException have been added GitHubService - handling ofHttpClientErrorException and ExceededRateLimitException have been added to catch block

--- src/main/java/com/atipera/okushyn/testassignment/exceptions/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/atipera/okushyn/testassignment/exceptions/RestResponseEntityExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
@@ -14,5 +15,17 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     protected ResponseEntity<ApiError> handleResourceNotFoundException(ResourceNotFoundException ex) {
         ApiError error = new ApiError(HttpStatus.NOT_FOUND, ex.getMessage());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    protected ResponseEntity<ApiError> handleHttpClientErrorException(HttpClientErrorException ex) {
+        ApiError error = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(ExceededRateLimitException.class)
+    protected ResponseEntity<ApiError> handleExceededRateLimitException(ExceededRateLimitException ex) {
+        ApiError error = new ApiError(HttpStatus.FORBIDDEN, ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
     }
 }
