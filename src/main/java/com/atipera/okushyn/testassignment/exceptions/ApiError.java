542d6a250f7e1e7e02ef81bfb641d1cd9f9a8e2c
kyshin <kyshin1993@gmail.com>
2024-08-29 13:05:58 +0200

    ApiError - has been changed to record

--- src/main/java/com/atipera/okushyn/testassignment/exceptions/ApiError.java
+++ src/main/java/com/atipera/okushyn/testassignment/exceptions/ApiError.java
@@ -1,14 +1,8 @@
 package com.atipera.okushyn.testassignment.exceptions;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.http.HttpStatusCode;
 
-@Getter
-@Setter
-@AllArgsConstructor
-public class ApiError {
-    private HttpStatusCode statusCode;
-    private String message;
+
+public record ApiError(HttpStatusCode statusCode, String message) {
+
 }
